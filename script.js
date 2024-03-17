9b37bd56f4d00fd1f4410ada112d2bcab62d5443
Vincde <145802178+Vincde@users.noreply.github.com>
2024-03-17 01:38:14 +0100

    Add first class Book and first external function
    Added class Book to the code. This class gets the name,author page etc. to produce a single book that inherits the library.This permits more availability for use cases

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
-//restart
+
+
+class Book{
+    constructor(title,author,nPages,redIt){
+        this.title = title;
+        this.author = author;
+        this.nPages = nPages;
+        this.redIt = redIt;
+    }
+
+    printBook(){
+        console.log(this.title + ' ' + this.author + ' ' + this.nPages + ' ' + this.redIt);
+    }
+
+    addToLibrary(){
+        Book.prototype.myLibrary.push(this);
+    }
+
+}
+
+
+function startLibrary(){
+    Book.prototype.myLibrary = [];
+
+    //Create a book?? 
+    //divide DOM from the rest
+    //when key is pressed
+    // a new book object is created
+    //this object then adds itself to the library array
+    
+}
