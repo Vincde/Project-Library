767455ed67ccbf2f7b2b7f45076f7a3382b10638
Vincde <145802178+Vincde@users.noreply.github.com>
2024-03-17 21:52:04 +0100

    Add changeRed and deleteSelf functions to Book class

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ class Book{
         this.nPages = nPages;
         this.redIt = redIt;
     }
+    
 
     printBook(){
         console.log(this.title + ' ' + this.author + ' ' + this.nPages + ' ' + this.redIt);
@@ -16,16 +17,42 @@ class Book{
         Book.prototype.myLibrary.push(this);
     }
 
+    changeRed(){
+        if(this.redIt === 'yes') this.redIt = 'no';
+        else this.redIt = 'yes';
+    }
+
+    deleteSelf(){
+        /*for loop into |
+                        v
+        Book.prototype.myLibrary[i]
+        if one of the books === this 
+            delete it??? 
+            idk if this is possible */
+    }
+
 }
 
 
 function startLibrary(){
     Book.prototype.myLibrary = [];
 
-    //Create a book?? 
-    //divide DOM from the rest
-    //when key is pressed
-    // a new book object is created
-    //this object then adds itself to the library array
+
+
+        //NEW FUNCTION MAYBE????????????????????????????????????????????
+
+    //book pressed!!!!!!!!!1
+
+    /* let name = prompt('title');
+       let author = prompt('author');
+       let nPages = prompt('npages');
+       let redIt = prompt('redit');  */
+
+    let newBook = new Book(title,author,nPages,redIt);
+    newBook.addToLibrary();
+
+
+    //Aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+
     
 }
