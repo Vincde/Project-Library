83b151d2eb6b02527c1f4878a061caf32bcc89b5
Vincde <145802178+Vincde@users.noreply.github.com>
2024-05-17 13:50:52 +0200

    Form refactor completed

--- script.js
+++ script.js
@@ -29,6 +29,8 @@ class Book{
 }
 
 (function DOMElem () {
+    let inputErrorArr = [];
+
     Book.prototype.myLibrary = [];
 
     const clickMeButton = document.querySelector('body > button');
@@ -42,9 +44,13 @@ class Book{
         }else{
             formToShow.style.visibility = 'visible';
         }
-    })
+    });
+
+    inputErrorArr = inputFormsErrors();
 
     createButton.addEventListener('click',() =>{
+        if(inputErrorArr[0] || inputErrorArr[1] || inputErrorArr[2] || inputErrorArr[3]) return;
+
         startLibrary(inputsForm[0].value,inputsForm[1].value,inputsForm[2].value,inputsForm[3].value);
     });
 })();
@@ -106,4 +112,51 @@ const displayBook = function() {
         elementsSelector[j+3].textContent = Book.prototype.myLibrary[i].redIt;
         j += 4;
     }
+}
+
+function inputFormsErrors(){
+    let res = [0,0,0,0];
+    const inputsForm = document.querySelectorAll('form input');
+
+    inputsForm[0].addEventListener("input", (event) => {
+        if(inputsForm[0].validity.tooShort){
+            inputsForm[0].setCustomValidity("You have to Enter a text >= 5 characters");
+            res[0] = 1;
+        }else{
+            inputsForm[0].setCustomValidity("");
+            res[0] = 0;
+        }
+    });
+
+    inputsForm[1].addEventListener("input", (event) => {
+        if(inputsForm[1].validity.tooShort){
+            inputsForm[1].setCustomValidity("You have to Enter a text >= 5 characters");
+            res[1] = 1;
+        }else{
+            inputsForm[1].setCustomValidity("");
+            res[1] = 0;
+        }
+    });
+
+    inputsForm[2].addEventListener("input", (event) => {
+        if((inputsForm[2].validity.tooShort)){
+            inputsForm[2].setCustomValidity("Form is too short");
+            res[2] = 1;
+        }else{
+            inputsForm[2].setCustomValidity("");
+            res[2] = 0;
+        }
+    });
+
+    inputsForm[3].addEventListener("input", (event) => {
+        if((inputsForm[3].validity.patternMismatch)){
+            inputsForm[3].setCustomValidity("Field does not match the right pattern!");
+            res[3] = 1;
+        }else{
+            inputsForm[3].setCustomValidity("");
+            res[3] = 0;
+        }
+    });
+
+    return res;
 }
